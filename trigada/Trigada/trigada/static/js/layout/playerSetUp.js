7c64ff0ca96e1a63bb7e557c860c371275fa69f5
stillouyng <stillouyng@yandex.ru>
2024-05-19 15:52:51 +0500

    Added player settings saver

--- trigada/Trigada/trigada/static/js/layout/playerSetUp.js
+++ trigada/Trigada/trigada/static/js/layout/playerSetUp.js
@@ -1,15 +1,25 @@
 const player = new Plyr('#player');
 player.volume = 0.05;
+player.speed = 1;
+player.loop = true;
 
 window.addEventListener("load", () => {
     // turn off preloader
     document.getElementById("preloader").style.display = "none";
 
+    //Get player settings
+    const settings = JSON.parse(localStorage.getItem("player_settings"));
+    try {
+        player.volume = settings['volume'];
+        player.speed = settings['speed'];
+    } catch (err) {}
+
     // Get song name. This is the biggest piece of dog shit...
     const song_name = player.source.split("/")[player.source.split("/").length - 1].split("-")[1].replace("_", "").split(".")[0];
     // Set currentTime and play
     try {
-        player.currentTime = parseInt(JSON.parse(localStorage.getItem(song_name))['currentTime']);
+        const song_data = JSON.parse(localStorage.getItem(song_name));
+        player.currentTime = parseInt(song_data['currentTime']);
     } catch (err) {
         player.currentTime = 0;
     }
@@ -19,9 +29,15 @@ window.addEventListener("load", () => {
     const allTimeInSeconds = (parseInt(allTime.split(":")[0]) * 60 ) + parseInt(allTime.split(":")[1]);
 
     const timePlayed = setInterval(() => {
+        player_settings = {
+            "volume": player.volume,
+            "speed": player.speed,
+        }
+
         json_data = {
             "currentTime": player.currentTime
         }
         localStorage.setItem(song_name, JSON.stringify(json_data));
+        localStorage.setItem("player_settings", JSON.stringify(player_settings));
     }, 1000);
 });
