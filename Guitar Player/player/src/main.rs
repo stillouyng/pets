ec348dbcda04bc048ae22137ddef6586c1606891
stillouyng <56676429+stillouyng@users.noreply.github.com>
2024-05-30 19:16:09 +0500

    Tested sleep function

--- Guitar Player/player/src/main.rs
+++ Guitar Player/player/src/main.rs
@@ -1,5 +1,7 @@
 use log::{info, warn, LevelFilter};
 use simple_logging::{log_to_file};
+use std::{thread, time};
+
 
 const LOG_FILE: &str = "logs.log";
 
@@ -14,5 +16,11 @@ fn test_1(msg: &str) {
 fn main() {
     let _ = log_to_file(LOG_FILE, LevelFilter::Info);
     test_0("Info message");
+    let sleep_duration = time::Duration::from_millis(2000);
+    let now = time::Instant::now();
+    println!("Sleep start: {:?}", now);
+    thread::sleep(sleep_duration);
+    let after = time::Instant::now();
+    println!("Sleep end: {:?}", after);
     test_1("Warning message");
 }
