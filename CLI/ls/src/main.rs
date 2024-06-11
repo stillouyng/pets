6b3e6469844fdd23bd4be07e73e875ea84612c70
stillouyng <56676429+stillouyng@users.noreply.github.com>
2024-06-11 14:39:51 +0500

    Added beauty help args reader

--- CLI/ls/src/main.rs
+++ CLI/ls/src/main.rs
@@ -1,19 +1,32 @@
+
 use std::{fs, path};
 use clap::{Parser};
 
 
-#[derive(Parser)]
+/// Program for shows files in directory
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct Properties {
-    #[arg(default_value = "None")]
-    param: String
+    /// Shows internal files in directories
+    #[arg(short, long, default_value = "None")]
+    flags: String,
+}
+
+fn remove_first_chars(str: String) -> String {
+    let mut chars = str.chars();
+    chars.next();
+    chars.next();
+    return chars.as_str().to_string()
 }
 
 fn print_path(filepath: path::PathBuf, with_iternal_files: bool) {
     let mut extend: &str = "file";
+    let filename: String = remove_first_chars(filepath.display().to_string());
     if filepath.clone().is_dir() {
         extend = "dir";
+
     }
-    println!("{0: <15} | {1: <4} | ", filepath.display(), extend);
+    println!("{0: <35} | {1: <4} | ", filename, extend);
     if filepath.clone().is_dir() && with_iternal_files {
         print_internal_files(filepath);
     }
@@ -29,38 +42,22 @@ fn print_internal_files(filepath: path::PathBuf) {
 
 fn print_sub_file(filepath: path::PathBuf) {
     let mut extend: &str = "file";
+    let filename = remove_first_chars(filepath.display().to_string());
     if filepath.is_dir() {
         extend = "dir";
     }
-    println!("– {0: <25} | {1: <4} | ", filepath.display(), extend);
-}
-
-fn print_help() {
-    let available_params = [
-        ("help", "Shows help"),
-        ("R", "Shows internal files in folders")
-    ];
-    println!("Use \"ls -- *param*\"");
-    for params in available_params {
-        println!("{0: <5} | {1}", params.0, params.1);
-    }
+    println!("– {0: <33} | {1: <4} | ", filename, extend);
 }
 
 fn main() {
     let files: fs::ReadDir = fs::read_dir(".\\").unwrap();
     let args: Properties = Properties::parse();
-    if args.param == "help" {
-        print_help();
-    } else if args.param == "None" || args.param == "R" {
-        for file in files {
-            let filepath: path::PathBuf = file.unwrap().path();
-            if args.param == "R" {
-                print_path(filepath, true);
-            } else {
-                print_path(filepath, false);
-            }
+    for file in files {
+        let filepath: path::PathBuf = file.unwrap().path();
+        if args.flags == "R" {
+            print_path(filepath, true);
+        } else {
+            print_path(filepath, false);
         }
-    } else {
-        panic!("Use ls -- help")
     }
 }
