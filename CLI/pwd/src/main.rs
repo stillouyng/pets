a7d6fb79fccfe76c733e94abb48c39f05e17e83d
stillouyng <56676429+stillouyng@users.noreply.github.com>
2024-06-11 14:46:14 +0500

    Added args

--- CLI/pwd/src/main.rs
+++ CLI/pwd/src/main.rs
@@ -1,7 +1,15 @@
 use std::{env, io::Result, path::PathBuf};
+use clap::{Parser};
+
+
+/// Program for print current path
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Properties {}
 
 
 fn main() -> Result<()> {
+    Properties::parse();
     let path: PathBuf = env::current_dir()?;
     println!("{}", path.to_string_lossy());
     Ok(())
